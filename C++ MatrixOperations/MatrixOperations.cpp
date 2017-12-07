48fd97a8c206fd602da6965a2bdb332a61ecc7cb
kunapavk <kunapavk@mail.uc.edu>
2017-12-07 17:38:20 -0500

    cleaned up C++ code

--- C++ MatrixOperations/MatrixOperations.cpp
+++ C++ MatrixOperations/MatrixOperations.cpp
@@ -1,39 +1,53 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include "Operations.h"
 using namespace std;
+
 int main() {
-	static const int arr[] = {{16},{2},{77},{29}};
+	srand(0);
+	//Play field to test the matrix operations
+	vector<vector<int>> x_10 = generateRandomMatrix(10, 10);
+	vector<vector<int>> y_10 = generateRandomMatrix(10, 10);
 
-	vector<vector<int>> vec(2, vector<int>(4));
-	
-	for (int j = 0; j < vec[0].size(); j++) {
-		for (int k = 0; k < vec.size(); k++) {
-			(vec[k])[j] = arr[j];
-			//vec.push_back(vector <int>());
-			//vec[i].push_back(arr[j]);
-		}
-		
-	}
-	vec[1][3] = 34;
-	Operations m = Operations();
-	vector<vector<int>> retval = m.Add(vec, vec);
-	vector<vector<int>> g = m.Transpose(vec);
-	vector<vector<int>> j = m.Multiply(vec, m.Transpose(vec));
-	/*for (int i = 0; i < retval.size(); i++) {
-		for (int k = 0; k < retval[i].size(); k++) {
-			cout << "Position" << " " << k << " " << retval[i][k] << endl;
-		}
-	}*/
-	/*for (int i = 0; i < g.size(); i++) {
-		for (int k = 0; k < g[i].size(); k++) {
-			cout << "Position" << " " << k << " " << g[i][k] << endl;
-		}
-	}*/
-	for (int i = 0; i < j.size(); i++) {
-		for (int k = 0; k < j[i].size(); k++) {
-			cout << "Position" << " " << k << " " << j[i][k] << endl;
-		}
-	}
+	vector<vector<int>> x_50 = generateRandomMatrix(50, 50);
+	vector<vector<int>> y_50 = generateRandomMatrix(100, 50);
 
+	vector<vector<int>> x_100 = generateRandomMatrix(100, 100);
+	vector<vector<int>> y_100 = generateRandomMatrix(100, 100);
+
+	Operations a = Operations();
+
+	/*Test Adding*/
+	// printMatrix(x_10);
+	// printMatrix(y_10);
+	// printMatrix(a.Add(x_10, y_10));
+
+	/*Test Transpose*/
+	// printMatrix(x_50);
+	// printMatrix(a.Transpose(x_50));
+
+	/*Test Multiplication*/
+	// printMatrix(x_10);
+	// printMatrix(y_10);
+	// printMatrix(a.Multiply(x_10, y_10));
+
+}
+
+vector<vector<int>> generateRandomMatrix(int x, int y) {
+	vector<vector<int>> ret_val = vector<vector<int>>(x, vector<int>(y));
+	for (int i = 0; i < ret_val.size(); i++ ) {
+		generate(ret_val[0].begin(), ret_val[0].end(), rand);
+	}
+	return ret_val;
+}
+
+void printMatrix(vector<vector<int>> x) {
+	for (int i = 0; i < x.size(); i++) {
+		cout << "[ ";
+		for (int j = 0; x[i].size(); j++) {
+			cout << x[i][j] << ", ";
+		}
+		cout << endl;
+	}
 }
